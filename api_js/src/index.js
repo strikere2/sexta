16295fa0d5c23c64c62028db05e3a4dd33d6b9e4
AlissonPF <alissonpatrickfarias@gmail.com>
2023-03-31 11:25:29 -0300

    Aula 31-03

--- api_js/src/index.js
+++ api_js/src/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 //Criando um servidor web através do express
 const app = express();
 
+
+//Configurar a aplicação para receber dados no formato JSON
+app.use(express.json());
+
 //Configurando uma requisição com o método GET para
 //a raiz da aplicação
 app.get("/", function(request, response){
@@ -13,14 +17,22 @@ app.get("/", function(request, response){
 
 //EXERCÍCIO - Criar uma nova rota/funcionalidade para receber uma
 //informação pela URL da requisição
-app.get("/:categoria", (request, response) => {
-    console.log(request.params);
-})
+app.get("/:categoria/:produto", (request, response) => {
+    console.log(request.params.categoria, request.params.produto);
+    const { categoria, produto } = request.params;
+    response.status(200).json({categoria, produto});
+
+});
 
 //EXERCÍCIO - Criar uma nova rota/funcionalidade com método POST para receber uma
 //informação pelo corpo do requidito
 //POSTMAN, INSOMNIA e RESTCLIENT
-
+app.post("/", (request, response) => {
+    response.status(201).json
+    (
+        {message : "Categoria cadastrada!", categoria : request.body}
+    );
+});
 
 
 //O comando listen roda a aplicação
